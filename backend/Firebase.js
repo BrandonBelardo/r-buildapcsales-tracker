db91632cd50af4fb2c2e80e8e55ffbd5a2b280bd
Brandon Belardo <brandonbelardo@yahoo.com>
2025-02-24 12:24:58 -0500

    Created login page outline and setup Firebase.js to use env vars

--- backend/Firebase.js
+++ backend/Firebase.js
@@ -4,6 +4,21 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
+};
+  
+  
+  // Initialize Firebase
+  
+  const app = initializeApp(firebaseConfig);
+
+
 // const firebaseConfig = process.env.NODE_ENV === 'production' ? {
 //   apiKey: "",
 //   authDomain: "",
