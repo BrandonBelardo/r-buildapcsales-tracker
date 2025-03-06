5f3617d7f08a78b3777e3000a5c01d82c3f4eea6
Brandon Belardo <brandonbelardo@yahoo.com>
2025-03-06 11:39:55 -0500

    Added tab title and icon

--- pages/_app.js
+++ pages/_app.js
@@ -33,17 +33,16 @@ export default function App({ Component, pageProps }) {
     return (
         <StateContext>
             <Head>
-                <title>MVP Starter</title>
+                <title>r/buildapcsales Tracker</title>
                 <meta name='description' content='Put a description here about your app' />
                 <meta name='robots' content='index, follow' />
                 <link rel="apple-touch-icon" sizes="180x180" href="/favicon_package/apple-touch-icon.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon_package/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon_package/favicon-16x16.png" />
                 <link rel="manifest" href="/favicon_package/site.webmanifest" />
                 <meta name="msapplication-TileColor" content="#da532c" />
                 <meta name="theme-color" content="#ffffff" />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin /> <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet" /> </Head>
+                <link rel="icon" type="image/png" href="/favicon.png" />
             <GlobalStyle />
             {isClient && <Component {...pageProps}/>}
         </StateContext>
