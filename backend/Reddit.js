30f07b9f281694035bc1e368b45861babd4c9da7
Brandon Belardo <brandonbelardo@yahoo.com>
2025-03-06 10:53:41 -0500

    Attempting to fix problem by adding user-agent header

--- backend/Reddit.js
+++ backend/Reddit.js
@@ -28,7 +28,11 @@ export const checkForNewPosts = async () => {
     */
     if (DEMO) {
         try {
-            const response = await fetch("https://www.reddit.com/r/buildapcsales/new.json");
+            const response = await fetch("https://www.reddit.com/r/buildapcsales/new.json", {
+                headers: {
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+                }
+            });
 
             console.log("Response Status:", response.status);
             console.log("Response Headers:", response.headers);
